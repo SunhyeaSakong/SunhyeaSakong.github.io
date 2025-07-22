80f6d190b053d3fb94f74f9f944b09eee6f81e71
Sunhyea Sakong <sakongsun777@yahoo.fr>
2025-07-22 13:54:35 +0200

    Update app.py
    numeric_col has been defined.

--- app.py
+++ app.py
@@ -4,7 +4,8 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 
 st.title("Hogwarts Data Explorer")
-uploaded_file = st.file_uploader("Upload your csv file", type=["csv")
+
+uploaded_file = st.file_uploader("Upload your csv file", type=["csv"])
 
 if uploaded_file is not None:
     df = pd.read_csv(uploaded_file)
@@ -12,14 +13,18 @@ if uploaded_file is not None:
     st.write("### Dataset Preview")
     st.dataframe(df.head())
 
-    if 'Hogwarts House' in df.columns:
-        x = st.selectbox("X-axis", numeric_cols)
-        y = st.selectbox("Y-axis", numeric_cols)
+    numeric_cols = df.select_dtypes(include='number).columns.tolist()
+
+    if 'Hogwarts House' in df.columns and in numeric_cols:
+        x = st.selectbox("Select X-axis", numeric_cols)
+        y = st.selectbox("Select y-axis", numeric_cols)
 
         st.write(f"### Scatterplot: {x} vs {y} by House")
         fig, ax = plt.subplots()
         sns.scatterplot(data=df, x=x, y=y, hue="Hogwarts House", ax=ax)
         st.pyplot(fig)
+    elif 'Hofwarts House' not in df.columns:
+        st.Warning("Your dataset contain 'Hogwarts House' colomn for this plot.")
     else:
-        st.warning("Your dataset must contain "Hogwarts House" colomn for this plot.")
+        st.warning("Your dataset must contain numeric columns for plotting.")
        
